a4d59ce319bb9660e7f676a7fb35414a3d51629e
Tobi Jackson <tjackson@academytrainee.com>
2020-07-17 10:18:33 +0100

    I tried conditionals for a grade system

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+mark=int(input("Please enter your mark: "))
+
+if mark >65:
+    if mark <85:
+        print("Not bad a Pass!!!!")
+    elif mark > 85:
+        print("Distinction!!!")
+
+else:
+    print("You fool you shall not Pass!!!")
