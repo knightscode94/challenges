1c20f17bdf9385984b34fa85e0b49b3af80a1bd5
Tobi Jackson <tjackson@academytrainee.com>
2020-07-18 16:34:58 +0100

    part two of the challenge of the day

--- timestable.py
+++ timestable.py
@@ -2,9 +2,16 @@
 Given an integer n, write a python function which returns a times table grid for all the integers between 1 and n.
 The grid should be separated by tabs and new lines.
 """
-n = int(input("Times table to: "))
-nu = n+1
-for x in range (1, nu):
-    for y in range (1, nu):
-        print (x*y, end=' ') 
-    print()
+def number():
+    nu = int(input("Times table to: "))
+    nu=nu+1
+    return nu
+
+def grid():
+    nu=number()
+    for x in range (1, nu):
+        for y in range (1, nu):
+            print (x*y, end='\t') 
+        print()
+
+grid()
