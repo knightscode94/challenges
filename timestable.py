4fe60a4f546449e746a28bcaa2e6b559785044ab
Tobi Jackson <tjackson@academytrainee.com>
2020-07-20 10:24:49 +0100

    the right way

--- timestable.py
+++ timestable.py
@@ -2,16 +2,16 @@
 Given an integer n, write a python function which returns a times table grid for all the integers between 1 and n.
 The grid should be separated by tabs and new lines.
 """
-def number():
-    nu = int(input("Times table to: "))
+def grid(nu):
     nu=nu+1
-    return nu
-
-def grid():
-    nu=number()
+    output = ""
     for x in range (1, nu):
         for y in range (1, nu):
-            print (x*y, end='\t') 
-        print()
+            output += str(x*y) + '\t' 
+        output += "\n"
+    
+    return output
 
-grid()
+
+nu = int(input("Times table to: "))
+print(grid(nu))
